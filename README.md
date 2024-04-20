c5597728401fb48fd1ccb8e136a8e761ca92d90f
Patrick de Azevedo <patrickdeazevedo@gmail.com>
2024-04-19 23:07:33 -0300

    Código inicial do frontend

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# EventReg_front
-Reposiório do FrontEnd de um App para Cadastro de Inscritos/Presentes em um Evento
+# FrontEnd do App EventReg
+
+Este projeto faz parte do **MVP de Desenvolvimento Full Stack Básico** elaborado pelo aluno Patrick de Azevedo Ferreira.
+
+O objetivo aqui é um aplicativo para registrar as pessoas que participam dos eventos organizados pela empresa Solus IT.
+Existem os presentes que já estavam previamente inscritos e aqueles que se inscrevem no momento do evento.
+
+---
+## Como executar
+
+Primeiro deve ser rodado o backend (api). Veja isso no arquivo README.md da pasta EventReg_api.
+Em seguida, basta abrir o arquivo index.html no seu browser preferido e integrar com a aplicação.
+
+Bons estudos!
+
+Patrick de Azevedo
+patrickdeazevedo@gmail.com
---
+++ scripts.js
@@ -0,0 +1,159 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  quantidade vai representar o cpf
+  valor vai representar o e-mail
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+  let url = 'http://127.0.0.1:5000/produtos';
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.produtos.forEach(item => insertList(item.nome, item.cpf, item.email))
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Chamada da função para carregamento inicial dos dados
+  --------------------------------------------------------------------------------------
+*/
+getList()
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar um item na lista do servidor via requisição POST
+  Onde se lê Product, leia-se Inscrito/Presente
+  quantidade vai representar o cpf
+  valor vai representar o e-mail
+  --------------------------------------------------------------------------------------
+*/
+const postItem = async (inputProduct, inputQuantity, inputPrice) => {
+  const formData = new FormData();
+  formData.append('nome', inputProduct);
+  formData.append('cpf', inputQuantity);
+  formData.append('email', inputPrice);
+
+  let url = 'http://127.0.0.1:5000/produto';
+  fetch(url, {
+    method: 'post',
+    body: formData
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  parent.appendChild(span);
+  console.log("Passei por aqui")
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover um item da lista de acordo com o click no botão close
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+  let close = document.getElementsByClassName("close");
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza que quer remover?")) {
+        div.remove()
+        deleteItem(nomeItem)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um item da lista do servidor via requisição DELETE
+  produto é o correspondente a inscrito/presente
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = (item) => {
+  console.log(item)
+  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar um novo item com nome, cpf e e-mail
+  input representa o nome do inscrito/presente
+  quantity vai representar o cpf
+  price vai representar o e-mail
+  --------------------------------------------------------------------------------------
+*/
+const newItem = () => {
+  let inputProduct = document.getElementById("newInput").value;
+  let inputQuantity = document.getElementById("newQuantity").value;
+  let inputPrice = document.getElementById("newPrice").value;
+
+  if (inputProduct === '') {
+    alert("Escreva o nome de um inscrito/presente!");
+  } else if (isNaN(inputQuantity)) {
+    alert("CPF precisa ser número!");
+  } else if (inputPrice === '') {
+    alert("Escreva um e-mail!");
+  } else {
+    insertList(inputProduct, inputQuantity, inputPrice)
+    postItem(inputProduct, inputQuantity, inputPrice)
+    alert("Item adicionado!")
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  nameProduct representa o nome do inscrito/presente
+  quantity representa o CPF
+  price representa o e-mail
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (nameProduct, quantity, price) => {
+  var item = [nameProduct, quantity, price]
+  var table = document.getElementById('myTable');
+  var row = table.insertRow();
+
+  for (var i = 0; i < item.length; i++) {
+    var cel = row.insertCell(i);
+    cel.textContent = item[i];
+  }
+  insertButton(row.insertCell(-1))
+  document.getElementById("newInput").value = "";
+  document.getElementById("newQuantity").value = "";
+  document.getElementById("newPrice").value = "";
+
+  removeElement()
+}
